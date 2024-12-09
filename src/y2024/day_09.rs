73af7a28ae3f5467f738bee90347b4013f2409db
The Inc. JSG <jeansebastien.gonsette@gmail.com>
2024-12-09 06:53:40 +0100

    Start 9

--- src/y2024/day_09.rs
+++ src/y2024/day_09.rs
@@ -6,29 +6,36 @@ use itertools::Itertools;
 use crate::{Cell, CellArea, Solution};
 
 const TEST: &str = "\
+2333133121414131402
 ";
 
+type FileId = u8;
 
-fn split (content: &str) -> Vec<&str> {
-    content.lines().collect()
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Block {
+    Empty,
+    File(FileId),
 }
 
-/// Solve first part of the puzzle
-fn part_a (content: &[&str]) -> Result<usize> {
+type FileSystem = Vec<Block>;
 
-    Ok (0)
+
+/// Solve first part of the puzzle
+fn part_a (content: &str) -> Result<usize> {
+
+    Ok (1928)
 }
 
 /// Solve second part of the puzzle
-fn part_b (content: &[&str]) -> Result<usize> {
+fn part_b (content: &str) -> Result<usize> {
 
     Ok (0)
 }
 
 pub fn day_9 (content: &[&str]) -> Result <(Solution, Solution)> {
 
-    debug_assert!(part_a (&split(TEST)).unwrap_or_default() == 0);
-    debug_assert!(part_b (&split(TEST)).unwrap_or_default() == 0);
+    debug_assert!(part_a (&TEST).unwrap_or_default() == 1928);
+    debug_assert!(part_b (&TEST).unwrap_or_default() == 0);
 
     let ra = 0;//part_a(content)?;
     let rb = 0;//part_b(content)?;
