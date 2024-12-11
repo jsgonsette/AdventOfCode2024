35f78e7e36a730635efbb23d212f834cdff2fc45
The Inc. JSG <jeansebastien.gonsette@gmail.com>
2024-12-11 09:18:04 +0100

    Progress on 2022 23

--- src/y2022/day_23.rs
+++ src/y2022/day_23.rs
@@ -1,7 +1,8 @@
+use std::cmp::PartialEq;
 use std::fmt::Display;
 use anyhow::*;
 use itertools::Itertools;
-use crate::Solution;
+use crate::{Cell, CellArea, Solution};
 
 const TEST: &str = "\
 ..............
@@ -23,101 +24,224 @@ fn split (content: &str) -> Vec<&str> {
     content.lines().collect()
 }
 
-#[derive(Copy, Clone, Debug)]
-enum Cell {
+/// The four possible displacements
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Direction {
+    North, South, East, West,
+}
+
+/// The content of the [PlayGround] at some coordinate
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum FieldCell {
     Empty,
     Elf,
 }
 
-struct PlayGround {
+/// A coordinate on the [PlayGround]
+type Coo = (isize, isize);
 
-    /// Playground's cells
-    cells: Vec<Cell>,
+/// Encodes a vote at some location
+#[derive(Copy, Clone, Debug, Default)]
+struct Vote {
 
-    /// Dimensions
-    width: usize,
-    height: usize,
+    /// Coordinate the elf at this location would like to go to
+    proposition: Option<Coo>,
+
+    /// Number of propositions for this location
+    target_count: usize,
 }
 
-/// To help debugging
-impl Display for PlayGround {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+struct PlayGround {
+    field: CellArea<FieldCell>,
+    votes: Vec<Vote>,
+    current_dir: Direction,
 
-        for y in 0..self.height {
-            let row: String = (0..self.width).map(|x| {
-                self.sample((x, y)).to_char()
-            }).join("");
+}
 
-            f.write_str("\n")?;
-            f.write_str(&row)?;
+impl Direction {
+
+    /// Get the direction to consider next
+    fn next(self) -> Direction {
+        match self {
+            Direction::North => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::East,
+            Direction::East => Direction::North,
         }
-        f.write_str("\n")
+    }
+
+    /// Coordinate increment by stepping in the given direction
+    fn step(&self) -> Coo {
+        match self {
+            Direction::North => (0, -1),
+            Direction::South => (0, 1),
+            Direction::West => (-1, 0),
+            Direction::East => (1, 0),
+        }
     }
 }
 
-impl Cell {
-    fn from_character (c: char) -> Option<Cell> {
+impl Default for FieldCell {
+    fn default() -> Self {
+        FieldCell::Empty
+    }
+}
+
+impl Cell for FieldCell {
+    fn from_character (c: char) -> Option<FieldCell> {
         match c {
-            '.' => Some(Cell::Empty),
-            '#' => Some(Cell::Elf),
+            '.' => Some(FieldCell::Empty),
+            '#' => Some(FieldCell::Elf),
             _ => None,
         }
     }
 
     fn to_char (&self) -> char {
         match self {
-            Cell::Empty => '.',
-            Cell::Elf => '#',
+            FieldCell::Empty => '.',
+            FieldCell::Elf => '#',
         }
     }
 }
 
+
 impl PlayGround {
 
-    /// New playground instance from puzzle file content
-    fn new(content: &[&str], margin: usize) -> Result<PlayGround> {
+    fn new (content: &[&str]) -> Result<PlayGround> {
+        let field = CellArea::new(content)?.inflated(10);
+        let votes = vec![Vote::default(); field.width() * field.height()];
 
-        let height = content.len() + margin*2;
-        let width = content[0].len() + margin*2;
-        let cells = Self::load_cell_from_content(content, margin)?;
-
-        Ok(PlayGround { cells, width, height, })
+        Ok(PlayGround { field, votes, current_dir: Direction::North })
     }
 
-    /// Create the vector of cells used to encode the initial playground from the puzzle file `content`
-    fn load_cell_from_content (content: &[&str], margin: usize) -> Result<Vec<Cell>> {
+    fn resolve_votes (&mut self) -> usize {
 
-        let width = content[0].len();
+        let mut new_field = CellArea::<FieldCell>::new_empty(
+            self.field.width(),
+            self.field.height()
+        );
 
-        let margin_top = vec!["."; width].join("");
-        let margin_left = vec!["."; margin].join("");
+        let mut top = self.field.height();
+        let mut bottom = 0;
+        let mut left = self.field.width();
+        let mut right = 0;
+        let mut elf_count = 0;
 
-        let it_top = std::iter::repeat(margin_top.as_str()).take (margin);
-        let it_content = content.iter ().copied();
-        let it_bottom = std::iter::repeat(margin_top.as_str()).take (margin);
-        let it = it_top.chain(it_content).chain(it_bottom);
+        for (x, y, cell) in self.field.iter_cells() {
+            let x = x as isize;
+            let y = y as isize;
 
-        // Make a single vector of cells to encode the maze
-        let cells: Option<Vec<Cell>> = it.flat_map (|row| {
+            if *cell == FieldCell::Elf {
+                elf_count += 1;
 
-            let it_left = margin_left.as_bytes().iter();
-            let it_row = row.as_bytes().iter();
-            let it_right = margin_left.as_bytes().iter();
+                let new_pos = if let Some (target_coo) = self.vote_result((x, y)) {
+                    target_coo
+                } else {
+                    (x, y)
+                };
+                *new_field.sample_mut((new_pos.0 as usize, new_pos.1 as usize)) = FieldCell::Elf;
 
-            it_left.chain(it_row).chain(it_right).map(|&b| {
-                if row.len() == width {
-                    Cell::from_character(b as char)
-                }
-                else { None }
-            })
-        }).collect();
+                top = top.min(new_pos.1 as usize);
+                bottom = bottom.max(new_pos.1 as usize);
+                left = left.min(new_pos.0 as usize);
+                right = right.max(new_pos.0 as usize);
+            }
+        }
 
-        cells.ok_or(anyhow!("Invalid content"))
+        self.field = new_field;
+        self.current_dir = self.current_dir.next();
+
+        // Return the size of the empty area covered by the elves
+        (bottom-top+1)*(right-left+1) - elf_count
     }
 
-    /// Get the cell at some location `coo`
-    fn sample (&self, coo: (usize, usize)) -> Cell {
-        self.cells[coo.1 * self.width + coo.0]
+    fn vote_result (&self, coo: Coo) -> Option<Coo> {
+
+        // Get the target proposition
+        let vote = self.sample_vote(coo);
+        if let Some (coo_target) = vote.proposition {
+
+            // Check there is not collision there
+            let vote_target = self.sample_vote(coo_target);
+            if vote_target.target_count == 1 {
+                Some(coo_target)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    fn sample_vote (&self, coo: Coo) -> &Vote {
+        let idx = coo.1 as usize * self.field.width() + coo.0 as usize;
+        &self.votes [idx]
+    }
+
+    fn make_votes (&mut self) {
+        let mut votes = vec![Vote::default(); self.field.width() * self.field.height()];
+
+        for (x, y, cell) in self.field.iter_cells() {
+            let x = x as isize;
+            let y = y as isize;
+
+            // An Elf with a neighbor must make a proposition
+            if *cell == FieldCell::Elf && self.has_neighbors(x, y, None) {
+                self.make_vote(&mut votes, x, y);
+            }
+        }
+
+        self.votes = votes;
+    }
+
+    fn make_vote (&self, votes: &mut Vec<Vote>, x: isize, y: isize) {
+
+        // Test the 4 directions in sequence
+        let mut dir = self.current_dir;
+        for _ in 0..4 {
+
+            // Stop when the tested direction is empty and make a vote
+            if !self.has_neighbors(x, y, Some(dir)) {
+
+                // - the proposed coordinate
+                let step = dir.step();
+                let proposition = (x + step.0, y + step.1);
+
+                // - vote
+                let idx = y as usize * self.field.width() + x as usize;
+                votes [idx].proposition = Some (proposition);
+
+                let idx = proposition.1 as usize * self.field.width() + proposition.0 as usize;
+                votes [idx].target_count += 1;
+
+                //println!("Elf at {x},{y} proposes {},{}", proposition.0, proposition.1);
+                break;
+            }
+            dir = dir.next();
+        }
+    }
+
+    fn has_neighbors (&self, x: isize, y: isize, direction: Option<Direction>) -> bool {
+
+        let range_x = match direction {
+            None | Some(Direction::North) | Some(Direction::South) => x-1..=x+1,
+            Some(Direction::West) => x-1..=x-1,
+            Some(Direction::East) => x+1..=x+1,
+        };
+
+        let range_y = match direction {
+            None | Some(Direction::West) | Some(Direction::East) => y-1..=y+1,
+            Some(Direction::North) => y-1..=y-1,
+            Some(Direction::South) => y+1..=y+1,
+        };
+
+        let r = range_x.cartesian_product(range_y).any (|(xi, yi)| {
+            if xi == x && yi == y { return false}
+            self.field.try_sample((xi, yi)).copied() == Some (FieldCell::Elf)
+        });
+
+        //println!("Test {}, {} in dir {:?}: {}", x, y, direction, r);
+        r
     }
 }
 
@@ -125,9 +249,15 @@ impl PlayGround {
 /// Solve first part of the puzzle
 fn part_a (content: &[&str]) -> Result<usize> {
 
-    let playground = PlayGround::new(content, 10)?;
-    println!("{}", playground);
-    Ok(0)
+    let mut playground = PlayGround::new(content)?;
+
+    let mut empty_area = 0;
+    for _ in 0..10 {
+        playground.make_votes();
+        empty_area = playground.resolve_votes();
+    }
+
+    Ok(empty_area)
 }
 
 /// Solve second part of the puzzle
@@ -136,12 +266,12 @@ fn part_b (_content: &[&str]) -> Result<usize> {
     Ok(0)
 }
 
-pub fn day_23 (_content: &[&str]) -> Result <(Solution, Solution)> {
+pub fn day_23 (content: &[&str]) -> Result <(Solution, Solution)> {
 
-    debug_assert!(part_a (&split(TEST)).unwrap_or_default() == 0);
+    debug_assert!(part_a (&split(TEST)).unwrap_or_default() == 110);
     debug_assert!(part_b (&split(TEST)).unwrap_or_default() == 0);
 
-    let ra = 0;//part_a(content)?;
+    let ra = part_a(content)?;
     let rb = 0;//part_b(content)?;
 
     Ok((Solution::Unsigned(ra), Solution::Unsigned(rb)))
