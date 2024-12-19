f8468ba38a5abe8fcf4ca2eb1dd503504b2807cd
Jean Sebastien Gonsette <jean-sebastien.gonsette@aisin-europe.com>
2024-12-19 11:15:41 +0100

    Prepare 2024 20

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ fn main() -> Result<()> {
 
     solve_year(Y2022, 21..21);
     solve_year(Y2023, 10..10);
-    solve_year(Y2024, 19..20);
+    solve_year(Y2024, 19..21);
 
     //let result = benchmark_year(&Y2024);
     //dbg!(&result);
