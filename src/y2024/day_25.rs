be2620dea1d9e1a70217eea0e5d8797046527f35
The Inc. JSG <jeansebastien.gonsette@gmail.com>
2024-12-25 10:38:11 +0100

    Cleaning

--- src/y2024/day_25.rs
+++ src/y2024/day_25.rs
@@ -1,62 +1,64 @@
 use anyhow::*;
-use crate::{Cell, CellArea, Solution};
+use crate::Solution;
 
-const TEST: &str = "\
-";
-
+/// A key, with its height in 5 positions
 type Key = [u8; 5];
 
+/// A Lock, with its height in 5 positions
 type Lock = [u8; 5];
 
-fn split (content: &str) -> Vec<&str> {
-    content.lines().collect()
+/// Given the 5 first chars in `row`, increment the `heights` at the corresponding
+/// positions for each encountered `#`.
+fn inc_height_from_row_it (heights: &mut [u8; 5], row: &str) {
+    for (idx, b) in row.as_bytes() [0..5].iter().enumerate() {
+        match b {
+            b'#' => heights[idx] += 1,
+            b'.' => { },
+            _ => panic!(),
+        }
+    }
 }
 
+/// Load a [Key] from its description in the puzzle file content `rows`.
 fn load_key (rows: &[&str]) -> Key {
-    let mut key = Key::default();
 
+    let mut key = Key::default();
     for &row in rows [0..5].iter().rev() {
-        for (idx, b) in row.as_bytes() [0..5].iter().enumerate() {
-            match b {
-                b'#' => key[idx] += 1,
-                b'.' => { },
-                _ => panic!(),
-            }
-        }
+        inc_height_from_row_it (&mut key, row);
     }
 
     key
 }
 
+/// Load a [Lock] from its description in the puzzle file content `rows`.
 fn load_lock (rows: &[&str]) -> Lock {
     let mut lock = Lock::default();
 
     for &row in rows [0..5].iter() {
-        for (idx, b) in row.as_bytes() [0..5].iter().enumerate() {
-            match b {
-                b'#' => lock[idx] += 1,
-                b'.' => { },
-                _ => panic!(),
-            }
-        }
+        inc_height_from_row_it (&mut lock, row);
     }
 
     lock
 }
 
+/// Loads [Key]s and [Lock]s from the puzzle file content
 fn load_keys_and_locks (content: &[&str]) -> Result<(Vec<Key>, Vec<Lock>)> {
 
     let mut keys = Vec::<Key>::new();
     let mut locks = Vec::<Lock>::new();
 
+    // Each key or lock is exactly 7 rows height, + one empty line in between
     for idx in (0..content.len()).filter(|&i| i % 8 == 0) {
 
+        // A key starts with this empty pattern
         if content [idx] == "....." {
             keys.push(load_key(&content [idx+1..]));
         }
+        // A lock starts with this plain pattern
         else if content[idx] == "#####" {
             locks.push(load_lock(&content [idx+1..]));
         }
+        // Anything else is an error
         else {
             bail!("Invalid key or lock head: {}", content[idx]);
         }
@@ -65,7 +67,7 @@ fn load_keys_and_locks (content: &[&str]) -> Result<(Vec<Key>, Vec<Lock>)> {
     Ok((keys, locks))
 }
 
-
+/// Check if a key and a lock fit with each other, (there must have no overlap)
 fn fit_key_and_lock (key: &Key, lock: &Lock) -> bool {
 
     if key [0] + lock [0] > 5 { false }
@@ -79,35 +81,22 @@ fn fit_key_and_lock (key: &Key, lock: &Lock) -> bool {
 /// Solve first part of the puzzle
 fn part_a (content: &[&str]) -> Result<usize> {
 
+    // Load the keys and locks
     let (keys, locks) = load_keys_and_locks(content)?;
 
+    // Count the number of fits
     let mut num_fits = 0;
-
     for key in keys.iter () {
         for lock in locks.iter () {
             if fit_key_and_lock(&key, &lock) { num_fits += 1; }
         }
     }
-    println!("Number of keys: {}", keys.len());
-    println!("Number of locks: {}", locks.len());
-    println!("Number of fits: {}", num_fits);
 
-    Ok(0)
-}
-
-/// Solve second part of the puzzle
-fn part_b (_content: &[&str]) -> Result<usize> {
-
-    Ok(0)
+    Ok(num_fits)
 }
 
 pub fn day_25 (content: &[&str]) -> Result <(Solution, Solution)> {
 
-    debug_assert!(part_a (&split(TEST)).unwrap_or_default() == 0);
-    debug_assert!(part_b (&split(TEST)).unwrap_or_default() == 0);
-
     let ra = part_a(content)?;
-    let rb = 0;//part_b(content)?;
-
-    Ok((Solution::Unsigned(ra), Solution::Unsigned(rb)))
+    Ok((Solution::Unsigned(ra), Solution::Unsigned(0)))
 }
